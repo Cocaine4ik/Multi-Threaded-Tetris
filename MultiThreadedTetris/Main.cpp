c6009d295770a581cf83774939d9aff8e12f27ff
Cocaine <kokotkin.alexander@gmail.com>
2023-08-10 17:42:39 +0300

    add notes

--- MultiThreadedTetris/Main.cpp
+++ MultiThreadedTetris/Main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "Game.h"
 
+// 1. Tetromino rotation
+// 2. DONE || Building only if touch on down side
+// 3. Clear line if line built and move down no 1 point all built blocks
+// 4. Thread for calculating score
+// 5. Change and return speed to defaul if down pressed
+
+
 int main()
 {
     Game game;
